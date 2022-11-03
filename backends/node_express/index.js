3b215dd6576e89625b2208ac22cdf5c1d6f33a9f
SirusCodes <hey@darshanrander.com>
2022-11-03 19:03:58 +0530

    feat: add file upload and json parse in node

--- backends/node_express/index.js
+++ backends/node_express/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const expressFileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 8080;
 const hostname = "127.0.0.1";
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb", strict: false }));
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
@@ -12,6 +13,17 @@ app.get("/", (_, res) => {
 
 app.post("/echo", (req, res) => {
   res.send(JSON.stringify({ response: `Hello, ${req.body.name}!` }));
+});
+
+app.post("/json_obj", (req, res) => {
+  const data = req.body;
+  res.send(`${data.length}`);
+});
+
+app.post("/file_upload", expressFileUpload({ limits: {} }), (req, res) => {
+  const file = req.files.benchmark;
+
+  res.send(`${file.size}`);
 });
 
 app.listen(port, hostname, () => {

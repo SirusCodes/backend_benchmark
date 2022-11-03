f38b9421651a6653f97fdd28d060b0024d2f0875
SirusCodes <hey@darshanrander.com>
2022-11-03 19:30:01 +0530

    feat: add file upload and json parse in shelf

--- backends/dart_shelf/bin/server.dart
+++ backends/dart_shelf/bin/server.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:compute/compute.dart';
+import 'package:shelf_multipart/form_data.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -8,7 +10,9 @@ import 'package:shelf_router/shelf_router.dart';
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
-  ..post('/echo', _echoHandler);
+  ..post('/echo', _echoHandler)
+  ..post("/file_upload", _fileUpload)
+  ..post("/json_obj", _jsonObj);
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!');
@@ -18,6 +22,25 @@ Future<Response> _echoHandler(Request request) async {
   final string = await request.readAsString();
   final body = json.decode(string);
   return Response.ok(json.encode({"response": "Hello, ${body["name"]}!"}));
+}
+
+Future<Response> _fileUpload(Request request) async {
+  final parameters = <String, String>{
+    await for (final formData in request.multipartFormData)
+      formData.name: await formData.part.readString(),
+  };
+
+  return Response.ok(parameters["benchmark"]?.length.toString());
+}
+
+Future<Response> _jsonObj(Request request) async {
+  final body = await request.readAsString();
+  final data = await compute(_asyncDecode, body);
+  return Response.ok(data.length.toString());
+}
+
+_asyncDecode(String obj) {
+  return jsonDecode(obj);
 }
 
 void main(List<String> args) async {
